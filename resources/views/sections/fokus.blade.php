da11689f4a8f44063cfee24dc0f8d2aa89f75b96
Arima1213 <arima3992@gmail.com>
2024-01-30 21:11:37 +0700

    perbaikan pada foku lagi

--- resources/views/sections/fokus.blade.php
+++ resources/views/sections/fokus.blade.php
@@ -1,66 +1,73 @@
-<section class="py-4 dark:bg-gray-800 dark:text-gray-100">
-	<div class="container mx-auto my-6 space-y-2 p-4 text-center">
-		<h2 class="text-5xl font-bold">5 Focus Area</h2>
-		<p class="dark:text-gray-900">ISCOM is a community that focuses on five key areas: UI/UX Design,
-			Programming, Internet of Things (IoT), Business Planning, and Data Science. These areas were
-			chosen strategically to cover essential aspects of information systems.</p>
-	</div>
-	<div
-		class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-sm lg:flex-row">
-		<img src="https://source.unsplash.com/640x480/?1" alt=""
-			class="aspect-video h-80 rounded-md bg-slate-50">
-		<div class="flex flex-1 flex-col justify-center rounded-2xl">
-			<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
-			<h3 class="text-3xl font-bold text-gray-950">UI/UX Design</h3>
-			<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Dolor aliquam possimus quas, error esse quos.</p>
+<section class="bg-slate-200">
+	<div class="mx-auto max-w-screen-xl px-6 py-4 text-gray-950">
+		<div class="container mx-auto my-6 space-y-2 p-4 text-center">
+			<h2 class="text-5xl font-bold">5 Focus Area</h2>
+			<p class="dark:text-gray-900">ISCOM is a community that focuses on five key areas: UI/UX Design,
+				Programming, Internet of Things (IoT), Business Planning, and Data Science. These areas were
+				chosen strategically to cover essential aspects of information systems.</p>
 		</div>
-	</div>
-	<div
-		class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-sm lg:flex-row-reverse">
-		<img src="https://source.unsplash.com/640x480/?2" alt=""
-			class="aspect-video h-80 rounded-md bg-slate-50">
-		<div class="flex flex-1 flex-col justify-center rounded-2xl">
-			<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
-			<h3 class="text-3xl font-bold text-gray-950">Programming</h3>
-			<p class="my-6 dark:text-gray-900">In ISCOM's programming focus, members start with the
-				fundamentals of web development, learning HTML, CSS, and JavaScript to build the foundation
-				necessary for creating web applications. The learning journey then advances to include
-				sophisticated frameworks like Next.js and Laravel. Next.js enhances web app performance with
-				server-side rendering, while Laravel simplifies complex backend development. </p>
+		<div
+			class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-xl lg:flex-row">
+			<img src="{{ asset('storage/uiux.webp') }}" alt=""
+				class="aspect-video h-80 rounded-md bg-slate-50 object-cover">
+			<div class="flex flex-1 flex-col justify-center rounded-2xl">
+				<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
+				<h3 class="text-3xl font-bold text-gray-950">UI/UX Design</h3>
+				<p class="my-6 dark:text-gray-900">User Interface (UI) and User Experience (UX) are essential in
+					digital system design, with UI concentrating on visual elements like buttons and layouts, and
+					UX focusing on the overall user experience. The learning covers aspects such as 'Style Guide'
+					for consistent design, 'Component' basics of UI, 'Landing Page Structure' for effective initial
+					interactions, 'Responsive Design' for versatility across devices, and 'Prototype' for design
+					testing. The aim is to create designs that are visually attractive, intuitive, and
+					user-efficient.</p>
+			</div>
 		</div>
-	</div>
-	<div
-		class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-sm lg:flex-row">
-		<img src="https://source.unsplash.com/640x480/?1" alt=""
-			class="aspect-video h-80 rounded-md bg-slate-50">
-		<div class="flex flex-1 flex-col justify-center rounded-2xl">
-			<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
-			<h3 class="text-3xl font-bold text-gray-950">Data</h3>
-			<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Dolor aliquam possimus quas, error esse quos.</p>
+		<div
+			class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-xl lg:flex-row-reverse">
+			<img src="{{ asset('storage/programming.webp') }}" alt=""
+				class="aspect-video h-80 rounded-md bg-slate-50 object-cover">
+			<div class="flex flex-1 flex-col justify-center rounded-2xl">
+				<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
+				<h3 class="text-3xl font-bold text-gray-950">Programming</h3>
+				<p class="my-6 dark:text-gray-900">In ISCOM's programming focus, members start with the
+					fundamentals of web development, learning HTML, CSS, and JavaScript to build the foundation
+					necessary for creating web applications. The learning journey then advances to include
+					sophisticated frameworks like Next.js and Laravel. Next.js enhances web app performance with
+					server-side rendering, while Laravel simplifies complex backend development. </p>
+			</div>
 		</div>
-	</div>
-	<div
-		class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-sm lg:flex-row-reverse">
-		<img src="https://source.unsplash.com/640x480/?2" alt=""
-			class="aspect-video h-80 rounded-md bg-slate-50">
-		<div class="flex flex-1 flex-col justify-center rounded-2xl">
-			<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
-			<h3 class="text-3xl font-bold text-gray-950">IoT</h3>
-			<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Dolor aliquam possimus quas, error esse quos.</p>
+		<div
+			class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-xl lg:flex-row">
+			<img src="{{ asset('storage/data.webp') }}" alt=""
+				class="aspect-video h-80 rounded-md bg-slate-50 object-cover">
+			<div class="flex flex-1 flex-col justify-center rounded-2xl">
+				<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
+				<h3 class="text-3xl font-bold text-gray-950">Data</h3>
+				<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Dolor aliquam possimus quas, error esse quos.</p>
+			</div>
 		</div>
-	</div>
-	<div
-		class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-sm lg:flex-row">
-		<img src="https://source.unsplash.com/640x480/?1" alt=""
-			class="aspect-video h-80 rounded-md bg-slate-50">
-		<div class="flex flex-1 flex-col justify-center rounded-2xl">
-			<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
-			<h3 class="text-3xl font-bold text-gray-950">Bisnis Plan</h3>
-			<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Dolor aliquam possimus quas, error esse quos.</p>
+		<div
+			class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-xl lg:flex-row-reverse">
+			<img src="{{ asset('storage/iot.webp') }}" alt=""
+				class="aspect-video h-80 rounded-md bg-slate-50 object-cover">
+			<div class="flex flex-1 flex-col justify-center rounded-2xl">
+				<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
+				<h3 class="text-3xl font-bold text-gray-950">IoT</h3>
+				<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Dolor aliquam possimus quas, error esse quos.</p>
+			</div>
+		</div>
+		<div
+			class="my-6 flex flex-col gap-5 overflow-hidden rounded-xl bg-slate-50 p-4 shadow-xl lg:flex-row">
+			<img src="{{ asset('storage/bisnisplan.webp') }}" alt=""
+				class="aspect-video h-80 rounded-md bg-slate-50 object-cover">
+			<div class="flex flex-1 flex-col justify-center rounded-2xl">
+				<span class="text-xs uppercase dark:text-gray-900">Join, it's free</span>
+				<h3 class="text-3xl font-bold text-gray-950">Bisnis Plan</h3>
+				<p class="my-6 dark:text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Dolor aliquam possimus quas, error esse quos.</p>
+			</div>
 		</div>
 	</div>
 </section>
